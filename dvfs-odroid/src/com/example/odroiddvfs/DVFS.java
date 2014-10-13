c19b887ca5fab5bc7098e7759376b6f7314b07ff
Yeo Kheng Meng <yeokm1@gmail.com>
2014-10-13 13:42:21 +0800

    moved code out to dvfs class

---
+++ dvfs-odroid/src/com/example/odroiddvfs/DVFS.java
@@ -0,0 +1,62 @@
+package com.example.odroiddvfs;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import android.util.Log;
+
+public class DVFS {
+
+	public static final String TAG = "DVFS";
+	
+	private IOStuff io;
+	private CPUStuff cpu;
+	private GPUStuff gpu;
+	
+	private ScheduledExecutorService scheduler;
+	public static final float TIME_INTERVAL_NANO_SECONDS = 1000000000;
+	
+	public DVFS(){
+		io = new IOStuff();
+		gpu = new GPUStuff(io);
+		cpu = new CPUStuff(io);
+	}
+	
+	
+	public void start(){
+		io.startShell();
+
+
+		scheduler = Executors.newSingleThreadScheduledExecutor();
+
+		scheduler.scheduleAtFixedRate
+		(new Runnable() {
+			public void run() {
+				float gpuUtil = gpu.getGPUUtilisation();
+				
+				double cpuUtil = cpu.getCPUUtilisation();
+
+				int fps = gpu.getFPS(TIME_INTERVAL_NANO_SECONDS);
+				
+				
+				Log.i(TAG, "FPS: " + Integer.toString(fps) + ", GPU: " + Float.toString(gpuUtil) + ", CPU: " + Double.toString(cpuUtil));
+
+			}
+		}, 0, 1000, TimeUnit.MILLISECONDS);
+	}
+	
+	public void stop(){
+		io.stopShell();
+
+
+		if(scheduler != null){
+			scheduler.shutdownNow();
+			scheduler = null;
+
+		}
+	}
+	
+	
+	
+}
--- dvfs-odroid/src/com/example/odroiddvfs/MainActivity.java
+++ dvfs-odroid/src/com/example/odroiddvfs/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.odroiddvfs;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,64 +9,26 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	public static final String TAG = "MainActivity";
-
-	public static final float TIME_INTERVAL_NANO_SECONDS = 1000000000;
-
-	private ScheduledExecutorService scheduler;
-	
-	private IOStuff io;
-	private GPUStuff gpu;
-	private CPUStuff cpu;
-	
+	private DVFS dvfs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		io = new IOStuff();
-		gpu = new GPUStuff(io);
-		cpu = new CPUStuff(io);
-
+		dvfs = new DVFS();
 	}
 
 	public void startButtonPress(View view){
 		Toast.makeText(this, "Started", Toast.LENGTH_SHORT).show();
-
-		io.startShell();
-
-
-		scheduler = Executors.newSingleThreadScheduledExecutor();
-
-		scheduler.scheduleAtFixedRate
-		(new Runnable() {
-			public void run() {
-				float gpuUtil = gpu.getGPUUtilisation();
-				Log.i(TAG + " GPU", Float.toString(gpuUtil));
-				
-				double cpuUtil = cpu.getCPUUtilisation();
-				Log.i(TAG + " CPU", Double.toString(cpuUtil));
-
-				int fps = gpu.getFPS(TIME_INTERVAL_NANO_SECONDS);
-				Log.i(TAG + " FPS", Integer.toString(fps));
-
-			}
-		}, 0, 1000, TimeUnit.MILLISECONDS);
-
+		dvfs.start();
 
 	}
 
 
 	public void stopButtonPress(View view){
 		Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
-		io.stopShell();
-
-
-		if(scheduler != null){
-			scheduler.shutdownNow();
-			scheduler = null;
-
-		}
+		dvfs.stop();
 	}
 
 
