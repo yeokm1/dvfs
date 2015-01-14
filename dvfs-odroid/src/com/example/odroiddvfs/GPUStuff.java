3ede4c855390525aa8d8065f59ed81e15ebabe93
Yeo Kheng Meng <yeokm1@gmail.com>
2015-01-14 14:18:51 +0800

    use index of last line instead of hardcoding 128 for FPS retrieval

--- dvfs-odroid/src/com/example/odroiddvfs/GPUStuff.java
+++ dvfs-odroid/src/com/example/odroiddvfs/GPUStuff.java
@@ -111,15 +111,17 @@ public class GPUStuff {
 		}
 
 		//First line is not used
+		
+		int indexOfLastLine = output.length - 1;
 
-		String lastLine = output[output.length - 1];
+		String lastLine = output[indexOfLastLine];
 		String[] split = splitLine(lastLine);
 		String lastFrameFinishTimeStr = split[2];
 
 		double lastFrameFinishTime = Double.parseDouble(lastFrameFinishTimeStr);
 		int frameCount = 0;
 
-		for(int i = 1; i <= 128 ; i++){
+		for(int i = 1; i <= indexOfLastLine ; i++){
 			String[] splitted = splitLine(output[i]);
 			String thisFrameFinishTimeStr = splitted[2];
 			double thisFrameFirstTime = Double.parseDouble(thisFrameFinishTimeStr);
