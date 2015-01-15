b73b3e6864787b005076d07bf0daf2448b7c3cd5
Yeo Kheng Meng <yeokm1@gmail.com>
2015-01-15 16:20:04 +0800

    foreground service

--- dvfs-odroid/src/com/example/odroiddvfs/MyApplication.java
+++ dvfs-odroid/src/com/example/odroiddvfs/MyApplication.java
@@ -1,11 +1,12 @@
 package com.example.odroiddvfs;
 
 import android.app.Application;
+import android.content.Intent;
 
 public class MyApplication extends Application {
 	 public void onCreate(){
-//		Intent keepAppIntent= new Intent(this, KeepAppAlive.class);
-//		startService(keepAppIntent);
+		Intent keepAppIntent= new Intent(this, KeepAppAlive.class);
+		startService(keepAppIntent);
 	 }
 	 
 
