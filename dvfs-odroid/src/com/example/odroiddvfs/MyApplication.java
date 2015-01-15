16ca18b36067e427040908f51d1a6605f5562dd8
Yeo Kheng Meng <yeokm1@gmail.com>
2015-01-15 12:39:55 +0800

    don't start service anymore

--- dvfs-odroid/src/com/example/odroiddvfs/MyApplication.java
+++ dvfs-odroid/src/com/example/odroiddvfs/MyApplication.java
@@ -1,12 +1,11 @@
 package com.example.odroiddvfs;
 
 import android.app.Application;
-import android.content.Intent;
 
 public class MyApplication extends Application {
 	 public void onCreate(){
-		Intent keepAppIntent= new Intent(this, KeepAppAlive.class);
-		startService(keepAppIntent);
+//		Intent keepAppIntent= new Intent(this, KeepAppAlive.class);
+//		startService(keepAppIntent);
 	 }
 	 
 
