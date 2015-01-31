e0fcf31d890e1a8a949350ac0cce3f21cbbbb0e7
Yeo Kheng Meng <yeokm1@gmail.com>
2015-01-31 17:25:38 +0800

    dvfs stuff

--- dvfs/jni/Android.mk
+++ dvfs/jni/Android.mk
@@ -4,5 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := dvfs
 LOCAL_SRC_FILES := dvfs.cpp
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
--- dvfs/jni/dvfs.cpp
+++ dvfs/jni/dvfs.cpp
@@ -1,1 +1,12 @@
 #include <jni.h>
+#include <android/log.h>
+
+#define APPNAME "DVFS"
+
+
+void
+Java_com_example_dvfs_DVFSNdk_startDVFS( JNIEnv* env,
+                                                  jobject thiz ){
+	__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "input %d", 2);
+}
+
