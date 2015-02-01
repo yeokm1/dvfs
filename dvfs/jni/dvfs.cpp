daa96e3f650521aa17639c43abb3e74b7150f27b
Yeo Kheng Meng <yeokm1@gmail.com>
2015-02-01 13:41:31 +0800

    regular runner

--- dvfs/jni/dvfs.cpp
+++ dvfs/jni/dvfs.cpp
@@ -7,6 +7,7 @@
 #include "cpu/CPUOdroid.h"
 
 #define CLASSNAME "DVFS-ndk"
+#define POLL_RATE_IN_MICROSECOND 1000000  //1 second
 
 
 int fpsLowbound;
@@ -17,6 +18,9 @@ pthread_t threadTask;
 
 void startDVFS(int _fpsLowBound, int _fpsHighBound, int _slidingWindowLength, bool _isPhoneNexus5);
 void * threadFunction(void *arg);
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1);
+
+void runThisRegularly();
 
 extern "C"
 JNIEXPORT void JNICALL
@@ -45,20 +49,51 @@ void startDVFS(int _fpsLowBound, int _fpsHighBound, int _slidingWindowLength, bo
 
 }
 
+//From http://stackoverflow.com/questions/1468596/calculating-elapsed-time-in-a-c-program-in-milliseconds
+/* Return 1 if the difference is negative, otherwise 0.  */
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1){
+    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+
+    return (diff<0);
+}
+
 void * threadFunction(void *arg){
 
 	CPUOdroid cpu;
 
-
+	struct timeval tvBegin, tvEnd, tvDiff;
 
 	while(dvfsInProgress){
-		__android_log_print(ANDROID_LOG_INFO, CLASSNAME, "Thread regular");
-		sleep(1);
+
+		gettimeofday(&tvBegin, NULL);
+
+		runThisRegularly();
+
+		gettimeofday(&tvEnd, NULL);
+
+		timeval_subtract(&tvDiff, &tvEnd, &tvBegin);
+
+		int elapsedTime = tvDiff.tv_usec;
+
+		//This is to calculate how much time to sleep before the next poll
+		if(elapsedTime < POLL_RATE_IN_MICROSECOND){
+			int timeToSleep = POLL_RATE_IN_MICROSECOND - elapsedTime;
+			usleep(timeToSleep);
+		}
+
+
 	}
 
 
 	return NULL;
 
+}
+
+
+void runThisRegularly(){
+	__android_log_print(ANDROID_LOG_INFO, CLASSNAME, "Thread regular");
 }
 
 
