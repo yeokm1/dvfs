68465cb322cf185858de0ed0eff4d36b9e3bf4df
Yeo Kheng Meng <yeokm1@gmail.com>
2015-02-07 20:46:42 +0800

    ability to get GPU utilisation for Nexus 5

--- dvfs/src/com/example/dvfs/gpu/GPUNexus5.java
+++ dvfs/src/com/example/dvfs/gpu/GPUNexus5.java
@@ -6,16 +6,17 @@ import java.util.Collections;
 import com.example.dvfs.IOStuff;
 
 public class GPUNexus5 extends GPU{
-	
+
 	public static final String POWER_SCALE_POLICY = "/sys/class/kgsl/kgsl-3d0/pwrscale/policy";
 	public static final String FILE_GPU_MAX_FREQ = "/sys/class/kgsl/kgsl-3d0/max_gpuclk";
 	public static final String FILE_GPU_AVAILABLE_FREQS = "/sys/class/kgsl/kgsl-3d0/gpu_available_frequencies";
+	public static final String FILE_GPU_UTIL = "/sys/class/kgsl/kgsl-3d0/gpubusy";
 
 	public GPUNexus5(IOStuff io) {
 		super(io);
 		setToNoGPUPolicy();
 	}
-	
+
 	public void setToNoGPUPolicy(){
 		io.setThisValueToThisFile("none", POWER_SCALE_POLICY);
 	}
@@ -36,7 +37,22 @@ public class GPUNexus5 extends GPU{
 
 	@Override
 	public float getGPUUtilisation() {
-		return 100;
+		try{
+			String output = IOStuff.getStringFromFile(FILE_GPU_UTIL).trim();
+			String[] splitted = output.split(" ");
+			int busyCycles = Integer.parseInt(splitted[0].trim());
+			int totalCycles = Integer.parseInt(splitted[1].trim());
+
+			if(totalCycles == 0){
+				return 0;
+			} else {
+				float util = (((float) busyCycles) / totalCycles) * 100;		
+				return util;
+			}
+		}catch (NumberFormatException e){
+			return 0;
+		}
+
 	}
 
 	@Override
