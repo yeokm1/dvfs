18964d26571ce2ee80e10dfa158fc8441cf90f02
Yeo Kheng Meng <yeokm1@gmail.com>
2015-03-03 01:14:00 +0800

    dvfs binary only

--- dvfs-binary/jni/dvfs-binary.cpp
+++ dvfs-binary/jni/dvfs-binary.cpp
@@ -7,7 +7,6 @@
 
 int main( int argc, char* argv[]){
 
-
 	bool useNew = true;
 
 	if(argc < 3){
--- dvfs/src/com/example/dvfs/DVFS.java
+++
@@ -1,9 +0,0 @@
-package com.example.dvfs;
-
-public interface DVFS {
-	
-	public void start(int fpsLowbBound, int fpsHighBound, int slidingWindowLength);
-	
-	public void stop();
-
-}
