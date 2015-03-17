b3bc7630b4b7dd8c986a123af667bb647527b83e
Yeo Kheng Meng <yeokm1@gmail.com>
2015-03-17 19:03:54 +0800

    braces position

--- dvfs-binary/jni/dvfs/DVFS.cpp
+++ dvfs-binary/jni/dvfs/DVFS.cpp
@@ -223,8 +223,7 @@ void DVFS::decideDynamicFPSTarget(){
 }
 
 
-double DVFS::mapRange(double a1,double a2,double b1,double b2,double s)
-{
+double DVFS::mapRange(double a1,double a2,double b1,double b2,double s){
 	return b1 + (s-a1)*(b2-b1)/(a2-a1);
 }
 
